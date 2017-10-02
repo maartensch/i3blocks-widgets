9dead56ad3ffa1f14d9ca1ce4662b9b7d1324b1e
Schermer,Maarten M.J <me@maartenschermer.com>
2017-10-02 22:54:22 +0200

    update

--- monitor/widgetmonloop.py
+++
@@ -1,9 +0,0 @@
-import time
-starttime=time.time()
-import widgetmon
-import lib.widgetmonlib as widgetmonlib
-
-
-while True:
-    print widgetmonlib.get(widgetmon.paths,widgetmon.excludedFiles)
-    time.sleep(10.0 - ((time.time() - starttime) % 10.0))
