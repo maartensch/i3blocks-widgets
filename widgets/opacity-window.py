70559e1a3706b65e2d7aaf5f3d15125efccd9376
Schermer,Maarten M.J <me@maartenschermer.com>
2018-02-25 11:58:47 +0100

    added opacity window + compton config

---
+++ widgets/opacity-window.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python  
+# -*- coding: utf-8 -*-  
+import sys
+from PyQt4.QtGui import *  
+from PyQt4.QtCore import *
+
+a = QApplication(sys.argv)  
+w = QWidget()  
+w.show()  
+a.exec_()
